ce69fd66dcaf4baa37d336b334decf184c02534a
Colin Cogle <colin@colincogle.name>
2019-04-16 00:49:32 -0400

    Committing version 1.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# PSTimeMachine Change Log
+
+## Version 1.0.1 (2019-04-15)
+ - Changed the log file name from global to script scope.
+ - The source path is now validated to make sure it is a valid path and that it exists.
+ - This script requires PowerShell 5 or newer, for hard link support, but it was possible to run this script on older versions.  The check is now enforced.
+ - Miscellaneous code cleanup.
+
+## Version 1.0 (2019-04-09)
+Initial release.
--- PSTimeMachine.ps1
+++ PSTimeMachine.ps1
@@ -1,13 +1,16 @@
 <#
 .NOTES
-PSTimeMachine.ps1 - Version 1.0
-(c) 2019 Colin Cogle <ccogle@vectro-networks.com>
+PSTimeMachine.ps1 - Version 1.0.1
+(c) 2019 Colin Cogle <colin@colincogle.name>
 
 This program is free software:  you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+.LINK
+https://github.com/rhymeswithmogul/PSTimeMachine
 
 .SYNOPSIS
 Creates versioned, deduplicated backups of a folder.
@@ -28,7 +31,7 @@ None.
 The file or folder to back up, recursively.  This may also be specified via the pipeline.
 
 .PARAMETER DestinationPath
-The folder in which to place the backed-up files.
+The folder in which to place the backed-up files.  If the destination folder does not exist, this script will attempt to create it.
 
 .PARAMETER FailIfOldBackupsAreMissing
 If the destination folder does not exist, or if previous backups cannot be found inside it, terminate the backup immediately. This can be used to strictly check if a destination disk is available before trying to copy files to it.
@@ -46,27 +49,34 @@ Suppress statistics after a successful backup.  Statistics are not reported for 
 PS C:\> .\PSTimeMachine.ps1 -SourcePath C:\Users\jdoe -DestinationPath D:\Backups
 #>
 
-#Require -Version 5
+#Requires -Version 5
 
 [CmdletBinding()]
 Param(
-	[Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true)]
+	[Parameter(Mandatory, Position=0, ValueFromPipeline=$true)]
 	[Alias("Source", "Path")]
+	[ValidateNotNullOrEmpty()]
+	[ValidateScript({Test-Path -Path $_})]
 	[String]$SourcePath,
 
-	[Parameter(Mandatory=$true, Position=1)]
+	[Parameter(Mandatory, Position=1)]
 	[Alias("Destination", "Target")]
+	[ValidateNotNullOrEmpty()]
 	[String]$DestinationPath,
 
 	[Switch]$FailIfOldBackupsAreMissing = $false,
-	
+
 	[Alias("CopyOnlyBackup")]
 	[Switch]$NoHardLinks = $false,
 
 	[Switch]$NoLogging = $false,
-	
+
 	[Switch]$NoStatistics = $false
 )
+
+# Save the old error preference.  It's rude to clobber the user's environment.
+New-Variable -Option Constant -Name OldErrorActionPreference -Value $ErrorActionPreference
+$ErrorActionPreference = "Stop"
 
 # We're going to create a folder to hold the backup, named for the current date and time.
 # Format everything except the year with leading zeroes.
@@ -75,20 +85,20 @@ New-Variable -Option Constant -Name FolderName -Value (("{0:yyyy}-{0:MM}-{0:dd}T
 
 # Start logging?
 If ($NoLogging) {
-	$global:LogFile = (New-TemporaryFile)
-	Start-Transcript -Path (($global:LogFile).Name)
+	$script:LogFile = (New-TemporaryFile)
+	Start-Transcript -Path (($script:LogFile).Name)
 }
 
 # Header
 Write-Output "Starting backup job at $Today."
 Write-Output "  Current Folder = $(Get-Location)"
 Write-Output "  Source         = $SourcePath"
-Write-Output "  Destination    = $(Join-Path $DestinationPath ($FolderName -CReplace '\.inProgress'))"
+Write-Output "  Destination    = $(Join-Path -Path $DestinationPath ($FolderName -CReplace '\.inProgress'))"
 Write-Output "  Other Options  = $(If ($FailIfOldBackupsAreMissing){'NoFailIfOldBackupsAreMissing'}) $(If ($NoHardLinks){'NoHardLinks'}) $(If ($NoLogging){'NoLogging'}) $(If($NoStatistics){'NoStatistics'})`n"
 
 # Check to see if previous backups exist, if we are asked.
 :SanityCheck While ($FailIfOldBackupsAreMissing) {
-	Get-ChildItem $DestinationPath | ForEach-Object {
+	Get-ChildItem -Path $DestinationPath | ForEach-Object {
 		Write-Debug "Checking to see if $($_.Name) is a backup folder..."
 		If ($_.Attributes -CMatch "Directory" ) {
 			Break SanityCheck
@@ -97,7 +107,7 @@ Write-Output "  Other Options  = $(If ($FailIfOldBackupsAreMissing){'NoFailIfOld
 }
 
 # Create the backup destination.
-New-Item -Type Directory -Path (Join-Path $DestinationPath $FolderName) -Force | Out-Null
+New-Item -Type Directory -Path (Join-Path -Path $DestinationPath $FolderName) -Force | Out-Null
 
 # Keep some statistics.
 $bytesCopied = 0
@@ -107,7 +117,7 @@ Try {
 	# Has hardlinking support been disabled?  If so, just do a copy.
 	If ($NoHardLinks) {
 		Write-Verbose "Hard-linking is disabled at user request. Copying all files."
-		Copy-Item -Path (Join-Path $SourcePath "*") -Destination (Join-Path $DestinationPath $FolderName) -Recurse
+		Copy-Item -Path (Join-Path -Path $SourcePath "*") -Destination (Join-Path -Path $DestinationPath $FolderName) -Recurse
 	}
 	Else {
 		# Look for old backups in the same destination.
@@ -120,7 +130,7 @@ Try {
 				Throw [System.Management.Automation.ItemNotFoundException] "No previous backups were found. Exiting at user request."
 			}
 			Write-Verbose "No previous backups exist.  Creating an initial backup."
-			Copy-Item -Path (Join-Path $SourcePath "*") -Destination (Join-Path $DestinationPath $FolderName) -Recurse
+			Copy-Item -Path (Join-Path -Path $SourcePath "*") -Destination (Join-Path -Path $DestinationPath $FolderName) -Recurse
 		}
 		Else {
 			# Get the most recent backup name.
@@ -131,12 +141,12 @@ Try {
 
 				# Create directories, but compare files.
 				If ($_.Attributes -CMatch "Directory") {
-					Write-Debug "Creating folder $(Join-Path $DestinationPath $FolderName $RelativeSourceItemPath)"
-					New-Item -Type Directory -Path (Join-Path $DestinationPath $FolderName $RelativeSourceItemPath) | Out-Null
+					Write-Debug "Creating folder $(Join-Path -Path $DestinationPath $FolderName $RelativeSourceItemPath)"
+					New-Item -Type Directory -Path (Join-Path -Path $DestinationPath $FolderName $RelativeSourceItemPath) | Out-Null
 				}
 				Else {
 					# Compare file sizes and dates to determine if something has changed.
-					$PreviousCopyOfFile = Get-Item -Path (Join-Path $DestinationPath $PreviousBackup $RelativeSourceItemPath)
+					$PreviousCopyOfFile = Get-Item -Path (Join-Path -Path $DestinationPath $PreviousBackup $RelativeSourceItemPath)
 					If (($_.LastWriteTime -eq $PreviousCopyOfFile.LastWriteTime) -And ($_.Length -eq $PreviousCopyOfFile.Length)) {
 						# The file has not changed since the last backup.  Create a hard link.
 						$DestinationHardlink = @{
@@ -151,7 +161,7 @@ Try {
 					}
 					Else {
 						Write-Verbose "Copying: $RelativeSourceItemPath"
-						Copy-Item -Path $_.FullName -Destination (Join-Path $DestinationPath $FolderName $RelativeSourceItemPath) -ErrorAction Stop
+						Copy-Item -Path $_.FullName -Destination (Join-Path -Path $DestinationPath $FolderName $RelativeSourceItemPath) -ErrorAction Stop
 						$bytesCopied += $_.Length
 					}
 					$bytesTotal  += $_.Length
@@ -160,21 +170,24 @@ Try {
 		}
 	}
 	Write-Output "Backup completed at $(Get-Date)."
-	Rename-Item -Path (Join-Path $DestinationPath $FolderName) -NewName ($FolderName -CReplace '\.inProgress')
-	If (-Not $NoStatistics -and $bytesCopied -gt 0 -and $bytesTotal -gt 0) {
+	Rename-Item -Path (Join-Path -Path $DestinationPath $FolderName) -NewName ($FolderName -CReplace '\.inProgress')
+	If (-Not $NoStatistics -And $bytesTotal -gt 0) {
 		$pctCopied = $bytesCopied / $bytesTotal
 		Write-Output "Copied $($bytesCopied / 1048576) MB out of $($bytesTotal / 1048576) MB: $('{0:p0}' -f (1 - $pctCopied)) percent savings."
 	}
 }
 Catch {
 	Write-Error "Backup failed at $(Get-Date); removing in-progress backup!`nError: $($_.Exception.Message)"
-	Remove-Item -Recurse -Force (Join-Path $DestinationPath $FolderName)
+	Remove-Item -Recurse -Force (Join-Path -Path $DestinationPath $FolderName)
 }
 Finally {
-	If ($NoLogging) {
+	If (-Not $NoLogging) {
 		Write-Verbose "Moving log file to backup destination."
 		Stop-Transcript
-		Write-Debug "Moving $($_.FullName) to $(Join-Path $DestinationPath 'PSTimeMachine.log')"
-		Move-Item -Path (($global:LogFile).Name) -Destination (Join-Path $DestinationPath $FolderName "PSTimeMachine.log") -ErrorAction Continue
+		Write-Debug "Moving $($_.FullName) to $(Join-Path -Path $DestinationPath 'PSTimeMachine.log')"
+		Move-Item -Path (($script:LogFile).Name) -Destination (Join-Path -Path $DestinationPath $FolderName "PSTimeMachine.log") -ErrorAction Continue
 	}
+
+	# Restore the user's preferred error action preference.
+	$ErrorActionPreference = $OldErrorActionPreference
 }
